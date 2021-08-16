dfea29c1b2a7fd76f3fd63c20fac7ee0df5a3509
pedromiguelssilva <pedromiguelssilva24@gmail.com>
2021-08-16 16:23:11 +0100

    Refresh 2

---
+++ RunScript.bat
@@ -0,0 +1,5 @@
+call "C:\Users\pedro\anaconda3\Scripts\activate.bat"
+
+python "C:\Users\pedro\OneDrive\Ambiente de Trabalho\Pedro Silva\Linkedin Scrapping\linkedin-jobs-scrapper\01. DataExtraction.py" %*
+
+pause
