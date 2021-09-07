2a9381a0f6e2747e3a0192dfc60d4fc369494286
pedromiguelssilva <pedromiguelssilva24@gmail.com>
2021-09-07 15:52:10 +0100

    Removed test files from tracking

--- RunScript.bat
+++
@@ -1,5 +0,0 @@
-call "C:\Users\pedro\anaconda3\Scripts\activate.bat"
-
-python "C:\Users\pedro\OneDrive\Ambiente de Trabalho\Pedro Silva\Linkedin Scrapping\linkedin-jobs-scrapper\01. DataExtraction.py" %*
-
-pause
