94a51c543582035d3b5e0785209dffc65850fc98
pedromiguelssilva <pedromiguelssilva@F10289.oil.galpenergia.corp>
2023-01-05 16:51:51 +0000

    design production layout for the module

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+if __name__ == '__main__':
+
+    setup(
+        name = 'linkedin_jobs_scrapper',
+        version = '1.0',
+        author = 'Pedro Sousa Silva',
+        packages = find_packages()
+    )
