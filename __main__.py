454bcd67ab9eeed154b88eb8d08d3da5ba58e632
pedromiguelssilva <pedromiguelssilva@F10289.oil.galpenergia.corp>
2023-01-05 17:31:04 +0000

    laid out program structure with virtual function and variable names

--- __main__.py
+++ __main__.py
@@ -1,6 +1,21 @@
+from src.config import database
+from src.utils import gather_full_html, jobs_snapshot, create_database, append_to_database
+
+
 def main() -> None:
 
-    pass
+    # 1. gather jobs snapshot @ run time 
+    html = gather_full_html()
+    data = jobs_snapshot(html = html)
+
+    # 2. append to existing database if it exists already, else create it and then append the data
+    if database:
+        append_to_database(data = data)
+    else:
+        create_database()
+        append_to_database(data = data)
+
+    return None
 
 
 if __name__ == '__main__':
